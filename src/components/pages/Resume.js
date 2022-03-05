58b6bc1f15fbc20f5ddd307c6bbf7c17b91bd858
Phavy Chey <phavychey@Phavys-MacBook-Pro.local>
2022-03-05 00:30:05 -0800

    cards added, need to work on hooks

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "../../styles/Resume.css";
+import { Card, Button } from "react-bootstrap";
 
 function Resume() {
   return (
     <div>
       <h1>Resume</h1>
-    </div>
+
+    <Card style={{ width: '18rem' }}>
+    {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+    <Card.Body>
+      <Card.Title>Resume</Card.Title>
+      <Card.Text>
+        Please click the button below to download my resume.
+      </Card.Text>
+      <Button variant="primary"><a href="www.google.com">Click Here!</a></Button>
+    </Card.Body>
+  </Card>
+  </div>
   );
 }
 
